6019a8a71322f917584f7fce777c37e983b0f843
Lucas <lucas.fijen@gmail.com>
2019-04-07 12:36:32 +0200

    Created a df with average moods per day per user

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 #%%
 import pandas as pd
-print('hi')
+import datetime
+df = pd.read_csv('../dataset_mood_smartphone.csv', index_col=0)
+df['time'] = pd.to_datetime(df['time'])
+
+#Substracted 1 second from the time so 00:00 will be the last day)
+df['time'] -= datetime.timedelta(seconds=1)
+
+
+mooddf = df[df.variable == 'mood']
+# Create a df with the mean values of a mood per day
+meanmooddf = mooddf.groupby(['id',mooddf.time.dt.date]).mean().reset_index()
+print(meanmooddf)
+
 
 #%%
-print('test')
-
-lucas = 'lol'
-#%%
-lucas
