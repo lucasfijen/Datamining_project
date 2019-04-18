50797b7d75a1592d17879b369c8f8c7dd17ad707
Lucas <lucas.fijen@gmail.com>
2019-04-18 15:11:25 +0200

    Changed data_prep

--- data_prep.py
+++ data_prep.py
@@ -7,6 +7,7 @@ import numpy as np
 # Loading the database from a folder 1 hierarchy higher
 df = pd.read_csv('../dataset_mood_smartphone.csv', index_col=0)
 df['time'] = pd.to_datetime(df['time'])
+df = df.drop_duplicates()
 
 # Help function to split days
 def split_to_days(x, splitvalue):
@@ -23,6 +24,13 @@ df['subtractedtime'] = df.time.apply(lambda x: split_to_days(x, hourdaystarts))
 df['date'] = df['subtractedtime'].dt.date
 df['day'] = df['subtractedtime'].dt.weekday
 
+# NORMALISATION
+# print(df.variable.unique())
+# print(df[df.variable.isin(['circumplex.arousal', 'circumplex.valence'])].value * 100)
+
+# df[df.variable.isin(['circumplex.arousal', 'circumplex.valence'])].value = df[df.variable.isin(['circumplex.arousal', 'circumplex.valence'])].value * 1000
+# print(df[df.variable.isin(['circumplex.arousal', 'circumplex.valence'])].value)
+
 # SUM FOR ALL OTHERS
 sumdf = df[~df.variable.isin(['mood', 'circumplex.arousal', 'circumplex.valence', 'activity'])].groupby(['id', 'date', 'variable'])['value'].sum().unstack()
 sumdf.columns = 'sum_' + sumdf.columns
@@ -38,17 +46,41 @@ stddf.columns = 'std_' + stddf.columns
 # CONCAT IN FINAL DF
 final = pd.concat([meandf, stddf, sumdf], axis=1, sort=True)
 
+#Filling na with 0 for all sum columns
+final.loc[:, ~final.columns.isin(['mood', 'mean_activity', 'mean_circumplex.arousal', 'mean_circumplex.valence',
+       'mean_mood', 'std_activity', 'std_circumplex.arousal',
+       'std_circumplex.valence', 'std_mood'])] = final.loc[:, ~final.columns.isin(['mood', 'mean_activity', 'mean_circumplex.arousal', 'mean_circumplex.valence',
+       'mean_mood', 'std_activity', 'std_circumplex.arousal',
+       'std_circumplex.valence', 'std_mood'])].fillna(0)
+
+
+
 # INTERPOLATE MEAN MOOD
-final['interpolate_bool'] = final['mean_mood'].isnull().astype(int)
-final['interpol_mean_mood'] = final['mean_mood'].groupby(['id']).fillna(method='ffill')
+final['interpolate_mood_bool'] = final['mean_mood'].isnull().astype(int)
+final['mean_mood'] = final['mean_mood'].groupby(['id']).fillna(method='ffill')
+
+final['interpolate_arousal_bool'] = final['mean_circumplex.arousal'].isnull().astype(int)
+final['mean_circumplex.arousal'] = final['mean_circumplex.arousal'].groupby(['id']).fillna(method='ffill')
+
+final['interpolate_valence_bool'] = final['mean_circumplex.valence'].isnull().astype(int)
+final['mean_circumplex.valence'] = final['mean_circumplex.valence'].groupby(['id']).fillna(method='ffill')
+
+final['interpolate_activity_bool'] = final['mean_activity'].isnull().astype(int)
+final['mean_activity'] = final['mean_activity'].groupby(['id']).fillna(method='ffill')
 
 # SHIFT INTERPOLATED MEAN MOOD FOR TARGET COLUMN
-final['shifted_interpol_mean_mood'] = final.groupby(['id'])['interpol_mean_mood'].transform(lambda x:x.shift(-1))
+final['shifted_target_mood_bool'] = final.groupby(['id'])['interpolate_mood_bool'].transform(lambda x:x.shift(-1))
+final['target_mood'] = final.groupby(['id'])['mean_mood'].transform(lambda x:x.shift(-1))
 
-print(final[['interpolate_bool', 'interpol_mean_mood', 'mean_mood']])
+# DROPPING THE NA's THAT STILL OCCUR, ARE THE DATA BEFORE THE FIRST 
+# OCCURANCE OF THE VALUE, THUS THE ONES WE CANT INTERPOLATE
+final.dropna(axis=0, how='any', inplace=True)
 
-sumdf#%%
+#%%
 final.to_pickle('database_basic.pkl')
 print('Saved in database_basic.pkl')
+
+#%%
+final['mean_circumplex.arousal']
 
 #%%
