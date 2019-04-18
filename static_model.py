98753001510045b9856c5f54149a8911f1c57e05
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2019-04-18 16:52:21 +0200

    static model

--- static_model.py
+++ static_model.py
@@ -3,6 +3,8 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.svm import SVR
+import numpy as np
 
 database = pd.read_pickle('database_basic.pkl')
 database.head()
@@ -12,23 +14,25 @@ database.head()
 # [Original row + History row of N]
 def add_history_of_N_days(database, N, method='mean'):
     history_data = []
-    for row_id in range(database.shape[0]):
-        if row_id > 5: 
-            history = database.iloc[row_id-5:row_id, :]
-        elif row_id == 0:
-            mean_history = np.zeros((1, database.shape[1]))
-        else:
-            history = database.iloc[0:row_id, :]
-        if method == 'mean' and (row_id != 0):
-            mean_history = history.values.mean(axis=0)
-        history_data.append(mean_history)
+    for _, group in database.groupby(['id']):
+        for row_id in range(group.shape[0]):
+            if row_id > 5: 
+                history = database.iloc[row_id-5:row_id, :]
+            elif row_id == 0:
+                mean_history = np.zeros((1, database.shape[1]))
+            else:
+                history = database.iloc[0:row_id, :]
+            if method == 'mean' and (row_id != 0):
+                mean_history = history.values.mean(axis=0)
+            history_data.append(mean_history)
     return np.vstack(history_data)
 
 history = add_history_of_N_days(database.iloc[:, :], 5, method='mean')
 history = pd.DataFrame(history, dtype=float)
-# remove boolean from history
-
-df=df[cols]
 history.columns = 'history_' + database.columns
-new = pd.concat([database.iloc[:, :].reset_index(), history], axis=1)
+database_with_history = pd.concat([database.iloc[:, :].reset_index(), history], axis=1)
 
+#%% FIT SVM REGRESSOR
+
+clf = SVR()
+clf.
