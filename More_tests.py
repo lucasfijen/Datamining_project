23645b30b6abc711bd7ef5ccffd70eb77862f95c
JanZ <janzuiderveld@gmail.com>
2019-04-19 13:16:04 +0200

    Update More_tests.py

--- More_tests.py
+++ More_tests.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 df = pd.read_pickle('/Users/janzuiderveld/Documents/GitHub/database_basic.pkl')
 df.columns
 
-# LOAD DATA
+# LOAD DATA|
 df.head()
 
 
@@ -27,12 +27,49 @@ def convert_to_X_y(df):
     y = y.reshape((len(y), 1))
     return X, y
 
+TRAIN_P = 0.6
+VALID_P = 0.2
+TEST_P = 0.2
 
 
-#%% Benchmark
+X_train = []
+X_valid = []
+X_test = []
+y_train = []
+y_valid = []
+y_test = []
+
+train_lengths = []
+# length train + validation
+valid_lengths = []
+for _, group in df.groupby(['id']):
+    length = group.shape[0]
+    train_valid_split = int(length * TRAIN_P)
+    valid_test_split = int(length * (1 - TEST_P))
+
+    train_lengths.append(train_valid_split)
+    valid_lengths.append(valid_test_split)
+    y_train.append(group.iloc[:train_valid_split]['target_mood'].values)
+    y_valid.append(group.iloc[train_valid_split:valid_test_split]['target_mood'].values)
+    y_test.append(group.iloc[valid_test_split:]['target_mood'].values)
+
+    cols = [c for c in group.columns if ('bool' not in c) and ('target' not in c) and ('date' not in c) and ('id' not in c)]
+    group = group[cols]
+    
+    X_train.append(group.iloc[:train_valid_split]['mean_mood'].values)
+    X_valid.append(group.iloc[train_valid_split:valid_test_split]['mean_mood'].values)
+    X_test.append(group.iloc[valid_test_split:]['mean_mood'].values)
+
+#%% Benchmark 
 moods = df.mean_mood[df.interpolate_mood_bool == 0]
+
+lenMoods =(len(moods))
+# moodsTest = moods[-0.2 * lenMoods, -1]
+
+
 targetmoods = df.target_mood[df.interpolate_mood_bool == 0]
 mse = ((moods - targetmoods)**2).mean(axis=0)
+mse = ((np.array(X_test) - np.array(y_test))**2).mean(axis=0)
 se = (moods - targetmoods)**2
 print(mse)
 
@@ -59,7 +96,7 @@ stats.wilcoxon(moods[1:100], moods[101:200])
 
 X,y =convert_to_X_y(df)
 print(X.shape)
-pca = decomposition.PCA(n_components=10)
+pca = decomposition.PCA(n_components=28)
 pca.fit(X)
 X = pca.transform(X)
 print(X.shape)
