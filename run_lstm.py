8274af14385d98f13cabd528a1292fc7f515de81
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2019-04-19 10:57:06 +0200

    LSTM

--- run_lstm.py
+++ run_lstm.py
@@ -5,80 +5,83 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import pandas as pd
-from lstm_model import LSTM
+# from lstm_model import LSTM
 import numpy as np
+import matplotlib.pyplot as plt
 
-TRAIN_PERC = 0.7
-VALID_PERC = 0.2
-TEST_PERC = 0.1
+class LSTM(nn.Module):
 
-#%%
-# LOAD DATA
-all_data = pd.read_pickle('../database.pkl')
-all_data.head()
+    def __init__(self, input_dim, hidden_dim, batch_size, output_dim=1, num_layers=2):
+        super(LSTM, self).__init__()
+        # print("HELLO")
+        self.input_dim = input_dim
+        # print('test',self.input_dim,'test')
+        self.hidden_dim = hidden_dim
+        self.batch_size = batch_size
+        self.num_layers = num_layers
 
-#%%
-def convert_to_X_y(df):
-    ''' 
-    Splits df into X and y shape as numpy arrays
-    '''
-    X = df.loc[:, ~df.columns.isin(['targetmood'])].values
-    y = df.targetmood.values
-    y = y.reshape((len(y), 1))
-    return X, y
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
 
-#%%
-person_ids = all_data.index.levels[0]
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
 
-train, valid, test = [], [], []
+    def init_hidden(self):
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
 
-all_data = all_data.fillna(0)
+    def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # a = hidden, b = cellstate
+        # shape of self.hidden: (a, b), where a and b both 
+        # have shape (num_layers, batch_size, hidden_dim).
+        # print(input.size())
+        # seq_len, batch_size, embedding_size
+        # print(input.view(len(input), self.batch_size, 25))
+        # print('test2', len(input), self.batch_size, self.input_dim, 'test2')
+        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, self.input_dim))
+        
+        # print(lstm_out.size())
 
-# 80% of MOOD days into train set (so not just 80% of days)
-for person, days_group in all_data.groupby('id'):
-    # days_group = days_group.reset_index()
-    mood_df = days_group.dropna(subset=['targetmood']).reset_index()
-    first_mood_day = mood_df['date'].iloc[0]
-    # print('first_mood_day', first_mood_day)
-    last_mood_day = mood_df['date'].iloc[-1]
-    # print('last_mood_day', last_mood_day)
-    mood_interval = last_mood_day - first_mood_day
-    # print('mood_interval', mood_interval)
-    split_day_train_valid = first_mood_day + (mood_interval * TRAIN_PERC)
-    # print('split_day_train_valid',split_day_train_valid)
-    split_day_valid_test = first_mood_day + (mood_interval * (TRAIN_PERC + VALID_PERC))
-    # print('split_day_valid_test', split_day_valid_test)
-    idx = pd.IndexSlice
-    X, y = convert_to_X_y(days_group.loc[idx[:, :split_day_train_valid], :])
-    # y = days_group['targetmood'].loc[idx[:, :split_day_train_valid], :]
-    # t = days_group.loc[idx[:, :split_day_train_valid], :-1].values.reshape(-1, 1, 25)
-    # print(t.shape)
-    X = X.reshape(-1, 1, 25)
-    train.append(days_group.loc[idx[:, :split_day_train_valid], :])
-    valid.append(days_group.loc[idx[:, split_day_train_valid:split_day_valid_test], :])
-    test.append(days_group.loc[idx[:, split_day_valid_test:], :])
-
-train = pd.concat(train, axis=0, join='outer', copy=True)
-valid = pd.concat(valid, axis=0, join='outer', copy=True)
-test = pd.concat(test, axis=0, join='outer', copy=True)
-
-X_train, y_train = convert_to_X_y(train)
-X_valid, y_valid = convert_to_X_y(valid)
-X_test, y_test = convert_to_X_y(test)
-
-print(X_train.shape)
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
+        y_pred = self.linear(lstm_out)
+        
+        return y_pred.view(-1)
 
 #%%
 
-X_train_t = torch.from_numpy(X_train) #[:, :, np.newaxis]
-X_test_t = torch.from_numpy(X_test[:, :, np.newaxis])
-X_valid_t = torch.from_numpy(X_valid[:, :, np.newaxis])
+TRAIN_P = 0.6
+VALID_P = 0.2
+TEST_P = 0.2
 
-y_train_t = torch.from_numpy(y_train)
-y_test_t = torch.from_numpy(y_test)
-y_valid_t = torch.from_numpy(y_valid)
+database = pd.read_pickle('database_basic.pkl')
+database.head()
 
-lstm_input_size = train.shape[1]
+X_train = []
+X_valid = []
+X_test = []
+y_train = []
+y_valid = []
+y_test = []
+for _, group in database.groupby(['id']):
+    length = group.shape[0]
+    train_valid_split = int(length * TRAIN_P)
+    valid_test_split = int(length * (1 - TEST_P))
+    y_train.append(group.iloc[:train_valid_split]['target_mood'].values)
+    y_valid.append(group.iloc[train_valid_split:valid_test_split]['target_mood'].values)
+    y_test.append(group.iloc[valid_test_split:]['target_mood'].values)
+    cols = [c for c in group.columns if ('bool' not in c) and ('target' not in c) and ('date' not in c) and ('id' not in c)]
+    group = group[cols]
+    X_train.append(group.iloc[:train_valid_split].values)
+    X_valid.append(group.iloc[train_valid_split:valid_test_split].values)
+    X_test.append(group.iloc[valid_test_split:].values)
+
+#%% TRAIN
+
+lstm_input_size = X_train[0].shape[1]
 num_train = 1
 hidden = 30
 output_dim = 1
@@ -86,56 +89,71 @@ num_layers = 1
 learning_rate = 0.001
 num_epochs = 50
 
-model = LSTM(25, hidden, batch_size=num_train, output_dim=output_dim, num_layers=num_layers)
+lstm_model = LSTM(lstm_input_size, hidden, batch_size=num_train, output_dim=output_dim, num_layers=num_layers)
+lstm_model.hidden = lstm_model.init_hidden()
+optimiser = torch.optim.Adam(lstm_model.parameters(), lr=learning_rate)
+loss_fn = torch.nn.MSELoss()
 
-loss_fn = torch.nn.MSELoss(reduction='sum')
+train_losses = []
+valid_losses = []
+for e in range(100):
+    for i, sequence in enumerate(X_train):
+        # print(e, i, '/', len(X_train))
+        # print('length sequence:', sequence.shape[0])
+        # print('number of features:', sequence.shape[1])
+        lstm_model.zero_grad()
+        input = torch.from_numpy(sequence).float()
+        y_pred = lstm_model(input)
+        # print('length prediction sequence:', len(y_pred))
+        # print(y_pred)
 
-optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        our_loss = ((y_pred.data.numpy() - y_train[i])**2).mean()
+        loss = loss_fn(y_pred, torch.from_numpy(y_train[i]).float())
+
+        train_losses.append(loss)
+
+        # VALID
+        valid_loss = 0
+        for j in range(len(X_valid)):
+            input = torch.from_numpy(X_valid[j]).float()
+            y_pred = lstm_model(input)
+            our_loss = ((y_pred.data.numpy() - y_valid[j])**2).mean() 
+            valid_loss += our_loss 
+        valid_losses.append(valid_loss/len(X_valid))
+
+        # print('our loss', our_loss)
+        # print('MSEloss:', loss.item())
+        # Zero out gradient, else they will accumulate between epochs
+        optimiser.zero_grad()
+
+        # Backward pass
+        loss.backward()
+
+        # Update parameters
+        optimiser.step()
+
+plt.plot(train_losses, label='train')
+plt.plot(valid_losses, label='valid')
+plt.title('Loss')
+plt.legend()
+plt.show()
 
 #%%
+total_loss = 0
+for i in range(len(X_test)):
+    input = torch.from_numpy(X_test[i]).float()
+    # print(input.shape)
+    pred = lstm_model(input)
+    # print(y_test.shape
+    # print(len(y_test[i]))
+    total_loss += ((pred.data.numpy() - y_test[i])**2).mean()
+print('total test loss', total_loss.item()/len(X_test))
 
-# input_seq = torch.randn(20, 1, 25)
-
-input_seq = torch.from_numpy(X).float()
-# input_seq = input_seq.flo
-
-model.hidden = model.init_hidden()
-    
-# Forward pass
-y_pred = model(input_seq)
-print(y_pred)
-# loss = loss_fn(y_pred, 1)
-
-#%%
-#####################
-# Train model
-#####################
-
-hist = np.zeros(num_epochs)
-
-for t in range(num_epochs):
-    # Clear stored gradient
-    model.zero_grad()
-    
-    # Initialise hidden state
-    # Don't do this if you want your LSTM to be stateful
-    model.hidden = model.init_hidden()
-    
-    # Forward pass
-    y_pred = model(X_train_t)
-
-    loss = loss_fn(y_pred, y_train_t)
-    if t % 100 == 0:
-        print("Epoch ", t, "MSE: ", loss.item())
-    hist[t] = loss.item()
-
-    # Zero out gradient, else they will accumulate between epochs
-    optimiser.zero_grad()
-
-    # Backward pass
-    loss.backward()
-
-    # Update parameters
-    optimiser.step()
+# total_loss = 0
+# for i in range(len(X_valid)):
+#     input = torch.from_numpy(X_valid[i]).float()
+#     pred = lstm_model(input)
+#     total_loss += loss_fn(pred, torch.from_numpy(y_valid[i]).float())
+# print('total validation loss', total_loss.item()/len(X_valid))
 
 #%%
