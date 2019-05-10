2bd16f62c5132525f4e6c59d763bc1a26a130675
Lucas <lucas.fijen@gmail.com>
2019-05-10 16:28:33 +0200

    Click bias worked out

--- Assignment_2/data_reader.py
+++ Assignment_2/data_reader.py
@@ -16,7 +16,7 @@ except:
 print('Number of datapoints:', df.shape[0], 'Number of initial features:', df.shape[1], '\n')
 print('The features:', df.columns.values)
 
-#%%
+#%% Position bias relative chance of positions beeing clicked calculated
 random = df[df['random_bool'] == 1]
 non_random = df[df['random_bool'] == 0]
 
@@ -28,30 +28,25 @@ nonrandom_clicks = non_random.groupby('position')['click_bool'].mean()
 nonrandom_bookings = non_random.groupby('position')['booking_bool'].mean()
 nonrandom_ = pd.concat([nonrandom_clicks.rename('clicks'), nonrandom_bookings.rename('bookings')], axis=1)
 
-#%%
+#%% Calculate the relative chances of a click being performed due to click bias
 clicks_correction = random_clicks / nonrandom_clicks
 bookings_correction = random_bookings / nonrandom_bookings
 correction_df = pd.concat([random_clicks.rename('random clicks'), nonrandom_clicks.rename('non_random_clicks'), random_bookings.rename('random bookings'), nonrandom_bookings.rename('nonrandom bookings'), clicks_correction.rename('random/nonrandom clicks'), bookings_correction.rename('random/nonrandom bookings')], axis=1)
 correction_df.reset_index(inplace=True)
 correction_df.to_csv('position_bias_correction_df.csv')
 
-#%%
-# smalldf = df.head(10000)
-def correct_click(row):
-    position = row['position']
-    correction = correction_df[correction_df['position'] == position]['random/nonrandom clicks'].values[0]  
-    return row['click_bool'] * correction
-
-def correct_booking(row):
-    position = row['position']
-    correction = correction_df[correction_df['position'] == position]['random/nonrandom bookings'].values[0]  
-    return row['booking_bool'] * 5 * correction
-
-df['corrected_click_gain'] = df.apply(correct_click, axis=1)
-df['corrected_book_gain'] = df.apply(correct_booking, axis=1)
-df['corrected_total'] = df['corrected_click_gain'] + df['corrected_book_gain']
-
-df['non_corrected_total'] = df['click_bool'] + (5 * df['booking_bool'])
+#%% Perform merge to other dataframe
+temp_df = correction_df[['position', 'random/nonrandom clicks', 'random/nonrandom bookings']]
+newdf = df.merge(temp_df, on='position')
+print(newdf.head(20))
+#%% add the scorings and compensated scorings based on click bias
+newdf['corrected_click_gain'] = newdf.click_bool * (1 - newdf['random/nonrandom clicks'])
+newdf['corrected_book_gain'] = newdf.booking_bool * (1 - newdf['random/nonrandom bookings']) * 5
+newdf['corrected_total'] = newdf['corrected_click_gain'] + newdf['corrected_book_gain']
+newdf['non_corrected_total'] = newdf['click_bool'] + (5 * newdf['booking_bool'])
+print(newdf[(newdf.booking_bool == 1) & (newdf.position ==1) & (newdf.click_bool ==1)]['non_corrected_total'])
+print(df.shape)
+print(newdf.shape)
 
 #%% PLOT RANDOM VERSUS NON-RANDOM FRACTION OF CLICKS & BOOKINGS PER POSITION
 all = pd.concat([random_clicks.rename('random clicks'), nonrandom_clicks.rename('non random clicks'), random_bookings.rename('random bookings'), nonrandom_bookings.rename('non random_bookings')], axis=1)
@@ -61,12 +56,7 @@ all_plot.set_xlabel('position', fontsize=14)
 all_plot.set_title('Fraction of clicks and bookings at positions of the result page', fontsize=20)
 all_fig = all_plot.get_figure()
 all_fig.savefig('random_vs_nonrandom.png', dpi=200, facecolor='white')
-#%%
-# random_nonrandom_bookings = nonrandom_bookings - random_bookings
-# random_nonrandom_bookings.plot.bar(color='skyblue')
-#%%
-# random_nonrandom_clicks = nonrandom_clicks - random_clicks
-# random_nonrandom_clicks.plot.bar(color='skyblue')
+
 #%%
 # random_plot = random_.plot.bar(width=1, figsize=[14, 8], title='Random positioning')
 # random_fig = random_plot.get_figure()
@@ -75,54 +65,3 @@ all_fig.savefig('random_vs_nonrandom.png', dpi=200, facecolor='white')
 # nonrandom_fig = nonrandom_plot.get_figure()
 # nonrandom_plot = nonrandom_.plot.bar(width=1, figsize=[14, 8], title='Non-random positioning')
 # nonrandom_fig.savefig('nonrandom_position.png', dpi=200, facecolor='white')
-#%% Take a small subset because the dataset is BIG
-smalldf = df.head(10000)
-
-#%% The number of times a property occurs in the dataset
-df.prop_id.value_counts()
-
-#%% The number of countries is 172
-df.prop_country_id.value_counts()
-
-#%% 
-
-#%%
-# One srch_id corresponds to one result page of a search and
-# the variable that is to be predicted is the booking_bool (and click_bool?)
-
-# One result page of datapoints:
-# print(smalldf[smalldf['srch_id'] == 1])
-
-# The result pages are NOT of equal length:
-RP_lengths = []
-clicked = []
-booked = []
-clicked_and_booked = []
-for search_id in np.unique(smalldf['srch_id'].values):
-    RP_lengths.append(smalldf[smalldf['srch_id'] == search_id].shape[0])
-    clicked.append(len(smalldf[(smalldf['srch_id'] == search_id) & (smalldf['click_bool'] == 1)]))
-    booked.append(len(smalldf[(smalldf['srch_id'] == search_id) & (smalldf['booking_bool'] == 1)]))
-    clicked_and_booked.append(len(smalldf[(smalldf['srch_id'] == search_id) & (smalldf['booking_bool'] == 1) & (smalldf['click_bool'] == 1)]))
-
-#%% RESULT PAGE LENGTH (how many properties are shown for a user search)
-plt.hist(RP_lengths, bins=max(RP_lengths), density=True)
-plt.xlabel('Length of the result page (number of properties)')
-plt.ylabel('Percentage of searches')
-plt.show()
-
-#%% Per user search 1 or 0 hotels are booked (never more)
-print(np.unique(booked))
-plt.bar([0, 1], [len(booked) - np.count_nonzero(booked), np.count_nonzero(booked)])
-plt.show()
-
-#%% 
-print('Per user search :', np.unique(clicked), 'nr of times clicked, so never 0!')
-# plt.bar([0, 1], [len(clicked) - np.count_nonzero(clicked), np.count_nonzero(clicked)])
-plt.hist(clicked, density=True)
-plt.show()
-
-#%% BOOKED = CLICKED it seems (which makes sense)
-labels = ['booked', 'clicked', 'clicked_and_booked']
-plt.hist([booked, clicked, clicked_and_booked], density=True, label=labels)
-plt.legend()
-plt.show()
