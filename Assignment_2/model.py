a0b8f1fbf4b84bac4a9990ac8ab484c28d9c6efb
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2019-05-23 15:08:15 +0200

    model py changes

--- Assignment_2/model.py
+++ Assignment_2/model.py
@@ -11,6 +11,9 @@ import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 sns.set_palette("Paired")
 from position_bias import *
+from ndcg import *
+
+#%%
 
 df_train = pd.read_csv('prepped_df_train.csv')
 df_test = pd.read_csv('prepped_df_test.csv')
@@ -36,8 +39,8 @@ index_val = df_val[['srch_id', 'prop_id']]
 index_test = df_test[['srch_id', 'prop_id']]
 
 #%% Target = position (position based model)
-target_train = df_train[['corrected_position']].values
-target_valid = df_val[['corrected_position']].values
+target_train = df_train[['corrected_position']]
+target_val = df_val[['corrected_position']]
 
 #%%
 exclude = ['srch_id', 
@@ -54,9 +57,9 @@ train_exclude = ['total_non_corrected_gain',
             'gross_bookings_usd',
             'booking_bool']
 
-X_test = df_test.drop(exclude, axis=1).values
-X_train = df_train.drop(exclude+train_exclude, axis=1).values
-X_val = df_val.drop(exclude+train_exclude, axis=1).values
+X_test = df_test.drop(exclude, axis=1)
+X_train = df_train.drop(exclude+train_exclude, axis=1)
+X_val = df_val.drop(exclude+train_exclude, axis=1)
 
 print('df_train.shape', X_train.shape)
 print('df_val.shape', X_val.shape)
@@ -64,18 +67,19 @@ print('df_test.shape', X_test.shape)
 print(set(X_train.columns) - set(X_test.columns))
 print(set(X_val.columns) - set(X_test.columns))
 print(set(X_train.columns) - set(X_val.columns))
+
 #%% GBM imports
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
 
 #%% GBM 
 # Position model
-X_train_np = X_test.values()
-y_train_np = target_train.values()
-GBR = GradientBoostingRegressor()
-GBR.fit(X_train_np, y_train_np)
+GBR_position = GradientBoostingRegressor()
+GBR_position.fit(X_train.values, target_train.values.reshape(-1,))
 
-MSE = mean_squared_error(y[-100:], GBR.predict(X[-100:]))
+#%%
+valid_position_prediction = GBR_position.predict(X_val.values)
+MSE = mean_squared_error(target_val.values, valid_position_prediction)
 print("MSE: %.4f" % MSE)
 
 #%% STEP 2: QUALITY / GAIN MODEL
@@ -83,13 +87,42 @@ df_train = pd.read_csv('prepped_df_train.csv')
 df_test = pd.read_csv('prepped_df_test.csv')
 df_val = pd.read_csv('prepped_df_val.csv')
 
+df_train = df_train[df_train['random_bool'] == 0]
+df_val = df_val[df_val['random_bool'] == 0]
+df_test = df_test[df_test['random_bool'] == 0]
+
+
 #%% Target = position (position based model)
-target_train_gain = df_train[['total_corrected_gainNone']].values
-target_valid_gain = df_val[['total_corrected_gainNone']].values
+target_train_gain = df_train[['total_corrected_gainNone']]
+target_valid_gain = df_val[['total_corrected_gainNone']]
 
-X_test_gain = df_test.drop(exclude, axis=1).values
-X_train_gain = df_train.drop(exclude+train_exclude, axis=1).values
-X_val_gain = df_val.drop(exclude+train_exclude, axis=1).values
+X_test_gain = df_test.drop(exclude, axis=1)
+X_train_gain = df_train.drop(exclude+train_exclude, axis=1)
+X_val_gain = df_val.drop(exclude+train_exclude, axis=1)
 
-print(target_train_gain.shape, X_train_gain.shape)
+#%%
+# Gain model
+GBR_gain = GradientBoostingRegressor()
+GBR_gain.fit(X_train_gain.values, target_train_gain.values.reshape(-1,))
+
+#%%
+valid_gain_prediction = GBR_gain.predict(X_val_gain.values)
+MSE = mean_squared_error(target_valid_gain.values, valid_gain_prediction)
 print("MSE: %.4f" % MSE)
+
+#%%
+valid_position_prediction = (1. - valid_position_prediction) * 6
+#%%
+total_prediction = (0.5 * valid_position_prediction) + (0.5 * valid_gain_prediction)
+
+#%%
+total_df = pd.DataFrame(total_prediction, columns=['prediction'])
+total_df = pd.concat([total_df, df_val['total_non_corrected_gain'].rename('gain'), index_val], axis=1)
+
+#%%
+total_df.columns
+
+#%%
+print(perform_new_ndcg(total_df, 'prediction', 'gain'))
+
+#%%
