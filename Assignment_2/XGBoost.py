181d46271a2bf146987f7cd1cd095a08c86bb19d
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2019-05-24 12:58:40 +0200

    first prediction

--- Assignment_2/XGBoost.py
+++ Assignment_2/XGBoost.py
@@ -29,48 +29,96 @@ print(set(df_train.columns) - set(df_test.columns))
 print(set(df_val.columns) - set(df_test.columns))
 print(set(df_train.columns) - set(df_val.columns))
 
-#%% Index = srch_id, prop_id
-index_train = df_train[['srch_id', 'prop_id']]
-index_val = df_val[['srch_id', 'prop_id']]
-index_test = df_test[['srch_id', 'prop_id']]
-
-# Group sizes
-train_group_sizes = df_train.groupby(['srch_id']).size().values
-val_group_sizes = df_val.groupby(['srch_id']).size().values
-test_group_sizes = df_test.groupby(['srch_id']).size().values
 #%% STEP 1: POSTION BASED MODEL
 # Since position based, only take non-random to train on!
 X_train_pos = df_train[df_train['random_bool'] == 0]
-X_val_pos = df_val[df_val['random_bool'] == 0]
-X_test_pos = df_test[df_test['random_bool'] == 0]
+X_val_pos = df_val #[df_val['random_bool'] == 0]
+X_test_pos = df_test #[df_test['random_bool'] == 0]
 
-# Remove ....
+#%% Index = srch_id, prop_id
+index_train = X_train_pos[['srch_id', 'prop_id']].copy().values
+index_val = X_val_pos[['srch_id', 'prop_id']].copy().values
+index_test = X_test_pos[['srch_id', 'prop_id']].copy().values
+
+# Group sizes
+train_group_sizes = X_train_pos.groupby(['srch_id']).size().values
+val_group_sizes = X_val_pos.groupby(['srch_id']).size().values
+test_group_sizes = X_test_pos.groupby(['srch_id']).size().values
 
 #%% Target = position (position based model)
-y_train_pos = df_train[['corrected_position']].values
-y_val_pos = df_val[['corrected_position']].values
+y_train_pos = X_train_pos[['corrected_position']].values
+y_val_pos = X_val_pos[['corrected_position']].values
 
-train_dmatrix_pos = DMatrix(X_train_pos, y_train_pos)
-valid_dmatrix_pos = DMatrix(X_val_pos, y_val_pos)
-test_dmatrix_pos = DMatrix(X_test_pos)
+#%% DELETE, most importantly: 'total_corrected_gain_mean' & 'total_corrected_gain_std'
+exclude = ['srch_id', 
+            'prop_id',
+            'total_corrected_gain_mean', 
+            'total_corrected_gain_std'
+            ]
+train_exclude = ['total_corrected_gain', 'corrected_click_gain', 
+                 'total_non_corrected_gain', 'corrected_position', 
+                 'booking_bool', 'click_bool', 'position', 
+                 'gross_bookings_usd', 'corrected_book_gain'
+                 ]
+
+X_train_pos = X_train_pos.drop(exclude+train_exclude, axis=1)
+X_val_pos = X_val_pos.drop(exclude+train_exclude, axis=1)
+X_test_pos = X_test_pos.drop(exclude, axis=1)
+
+#%% FINAL CHECK
+print('X_train_pos.shape', X_train_pos.shape)
+print('X_val_pos.shape', X_val_pos.shape)
+print('X_test_pos.shape', X_test_pos.shape)
+print('y_train_pos.shape', y_train_pos.shape)
+print('y_val_pos.shape', y_val_pos.shape)
+print(set(X_train_pos.columns) - set(X_test_pos.columns))
+print(set(X_val_pos.columns) - set(X_test_pos.columns))
+print(set(X_train_pos.columns) - set(X_val_pos.columns))
+
+#%% Prepare matrices
+train_dmatrix_pos = DMatrix(X_train_pos.values, y_train_pos)
+valid_dmatrix_pos = DMatrix(X_val_pos.values, y_val_pos)
+test_dmatrix_pos = DMatrix(X_test_pos.values)
 
 train_dmatrix_pos.set_group(train_group_sizes)
 valid_dmatrix_pos.set_group(val_group_sizes)
 test_dmatrix_pos.set_group(test_group_sizes)
+
 #%% XGBOOST MODEL: POSITION-BASED
 #XGB model using efficient data structure Dmatrix
-params = {'max_depth':3,
+params_pos = {'max_depth':3,
 'min_child_weight':10,
 'learning_rate':0.3,
 'subsample':0.5,
 'colsample_bytree':0.6,
-'objective':'rank:pairwise',
+'objective':'rank:ndcg',
 'n_estimators':10000,
-'eta':0.3,
 'verbose':'True'}
 
-# train model
-xgb_model = xgb.train(params, train_dmatrix, evals=[(valid_dmatrix, 'validation')])
+# TRAIN
+xgb_model_pos = xgb.train(params_pos, train_dmatrix_pos, evals=[(valid_dmatrix_pos, 'validation')])
 
-# make predictions
-predict_xgb = xgb_model.predict(test_dmatrix)
+# VALIDATE
+predicted_val_pos = xgb_model_pos.predict(valid_dmatrix_pos)
+#%%
+print(predicted_val_pos.shape)
+
+#%%
+prediction_df = pd.DataFrame(predicted_val_pos, columns=['prediction'])
+corrected_pos_df = pd.DataFrame(y_val_pos, columns=['gain'])
+index_pos_df = pd.DataFrame(index_val, columns=['srch_id', 'prop_id'])
+total_df = pd.concat([prediction_df, corrected_pos_df, index_pos_df], axis=1)
+
+total_df.head()
+#%%
+print("Validation NDCG:", perform_new_ndcg(total_df, 'prediction', 'gain', ascending=True))
+#%% TEST
+predicted_test_pos = xgb_model_pos.predict(test_dmatrix_pos)
+#%%
+prediction_df = pd.DataFrame(predicted_test_pos, columns=['prediction'])
+index_pos_df = pd.DataFrame(index_test, columns=['srch_id', 'prop_id'])
+total_df = pd.concat([prediction_df, index_pos_df], axis=1)
+
+total_df.to_pickle('test_prediction_pos.pickle')
+#%%
+# print(perform_new_ndcg(total_df, 'prediction', 'gain', ascending=True))
--- Assignment_2/try_simple_submision.py
+++ Assignment_2/try_simple_submision.py
@@ -9,23 +9,23 @@ import pandas as pd
 import numpy as np
 from average_prop_dest_performance import *
 
-#%%
+# #%%
 
-df = pd.read_csv('data/training_set_VU_DM.csv')
-df_test = pd.read_csv('data/test_set_VU_DM.csv')
+# df = pd.read_csv('data/training_set_VU_DM.csv')
+# df_test = pd.read_csv('data/test_set_VU_DM.csv')
 
-#%% create gain and a gb with tupels
+# #%% create gain and a gb with tupels
 
-df['gain'] = df['click_bool'] + (5 * df['booking_bool'])
-df, gb = create_prop_dest_mean_performance(df, 'gain')
+# df['gain'] = df['click_bool'] + (5 * df['booking_bool'])
+# df, gb = create_prop_dest_mean_performance(df, 'gain')
 
-#%%
-testdf, gb = create_prop_dest_mean_performance(df_test, ['gain'], gb)
+# #%%
+# testdf, gb = create_prop_dest_mean_performance(df_test, ['gain'], gb)
 
+test_prediction = pd.read_pickle('test_prediction_pos.pickle')
+test_prediction.head()
 #%%
 from ndcg import make_submision
-testdf.columns
 #%%
-make_submision(testdf, 'gain', ascending=False, filename='data/testsub.csv')
-
+make_submision(test_prediction, 'prediction', ascending=True, filename='data/testsub.csv')
 #%%
