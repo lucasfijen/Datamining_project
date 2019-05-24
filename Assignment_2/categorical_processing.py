89db82a5f5fef9aab633220d6093828e5ee3d779
Lucas <lucas.fijen@gmail.com>
2019-05-24 12:59:29 +0200

    small memory changes

--- Assignment_2/categorical_processing.py
+++ Assignment_2/categorical_processing.py
@@ -3,19 +3,21 @@ import pandas as pd
 import numpy as np
 
 def add_descriptors(train_data, test_data, variable):
-    df_test_copy = test_data.copy()
-    df_train_copy = train_data.copy()
-    prep_categorical_numerical_descriptors(df_train_copy)
-    prep_categorical_numerical_descriptors(df_test_copy, 0)
+    df_test_copy = prep_categorical_numerical_descriptors(train_data)
+    df_train_copy = prep_categorical_numerical_descriptors(test_data, 0)
     all_numeric = pd.concat([df_train_copy, df_test_copy],axis=0)
+    del df_test_copy
+    del df_train_copy
     # print(all_numeric.columns)
+    print('Doing groupby')
     all_groupby = all_numeric.groupby(variable, sort=True).agg(['median', 'mean', 'std'])
     all_groupby_reset_index = all_groupby
+    del all_numeric
     # print(all_groupby_reset_index.head())
     all_groupby_reset_index.columns = ['_'.join(col).strip() for col in all_groupby_reset_index.columns.values]
     all_groupby_reset_index.fillna(-10, inplace=True)
     # print(all_groupby_reset_index.head())
-    
+    print('merging')
 
     newdf_train = train_data.merge(all_groupby_reset_index, 
                     how='left', 
@@ -23,9 +25,10 @@ def add_descriptors(train_data, test_data, variable):
     newdf_test = test_data.merge(all_groupby_reset_index, 
                     how='left', 
                     on=[variable])
-
-    newdf_train.drop([variable], axis=1)
-    newdf_test.drop([variable], axis=1)
+                    
+    del all_groupby_reset_index
+    newdf_train.drop([variable], axis=1, inplace=True)
+    newdf_test.drop([variable], axis=1, inplace=True)
 
     dont_delete = []
     for i in [2,3,5,8]:
@@ -60,9 +63,9 @@ def add_descriptors(train_data, test_data, variable):
 def prep_categorical_numerical_descriptors(data, train = 1):
 
     # remove describing cols if there
-    data.drop(list(data.filter(regex='std')), axis=1, inplace =True)
-    data.drop(list(data.filter(regex='mean')), axis=1, inplace =True)
-    data.drop(list(data.filter(regex='median')), axis=1, inplace =True)
+    # data.drop(list(data.filter(regex='std')), axis=1, inplace =True)
+    # data.drop(list(data.filter(regex='mean')), axis=1, inplace =True)
+    # data.drop(list(data.filter(regex='median')), axis=1, inplace =True)
     # print(data.columns)
 
     to_delete = [
@@ -93,7 +96,7 @@ def prep_categorical_numerical_descriptors(data, train = 1):
     #         c = 'comp' + str(i) + '_rate_percent_diff_mean'
     #         to_delete.extend([diff])
 
-    data.drop(to_delete, axis=1, inplace=True)
+    return data.drop(to_delete, axis=1).copy()
 
 
 
